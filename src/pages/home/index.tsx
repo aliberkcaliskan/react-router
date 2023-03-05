f019b6c7bd20ffa6c214dd33ce4af6c40fe51c95
Kaan Taş <sametkaan.tas0@gmail.com>
2023-03-06 01:18:40 +0300

    first commit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,28 @@
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import React from "react";
-import Title from '../../component/title'
+import '../../index.css';
+import SwiperCore, {Mousewheel,Controller} from 'swiper';
+import 'swiper/css';
+
+
+SwiperCore.use([Mousewheel,Controller]);
+
+
 export default function Home() {
-    return (
-        <Title fontSize="60px" title={'kaasdasdsadasan'} />
-    )
-}
+    const swiper = useSwiper();
+  return (
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      mousewheel
+      loop
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide><img src={require('../../images/slider/1.jpg')} alt="ilkslider" /></SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
+  );
+};
